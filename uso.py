cc6bc0c4d0a00799286d3f07c764850e7fd4b38a
User <zipper@jacobzipper.com>
2018-08-21 15:19:03 -0400

    rearranged hit objs!

--- uso.py
+++ uso.py
@@ -18,6 +18,30 @@ def copy(src, dest):
             shutil.copy(src, dest)
         else:
             print('Directory not copied. Error: %s' % e)
+def rearrangeTimingPoints(TIMING_OBJS):
+    for i in xrange(len(TIMING_OBJS) - 1):
+        break
+        # TODO REARRANGE TIMING POINTS
+def rearrangeHitObjs(HIT_OBJS):
+    HIT_OBJS = HIT_OBJS[::-1]
+    TOTAL_TIME = 0
+    longLast = int(HIT_OBJS[0].split(",")[-1].split(":")[0])
+    shortLast = int(HIT_OBJS[0].split(",")[2])
+    if longLast > shortLast:
+        TOTAL_TIME = longLast
+    else:
+        TOTAL_TIME = shortLast
+    for i in xrange(len(HIT_OBJS)):
+        cur = HIT_OBJS[i].split(",")
+        startTime = int(cur[2])
+        endTime = int(cur[-1].split(":")[0])
+        if endTime > startTime:
+            cur[2] = str(TOTAL_TIME - endTime)
+            cur[-1] = cur[-1].replace(str(endTime), str(TOTAL_TIME - startTime))
+        else:
+            cur[2] = str(TOTAL_TIME - startTime)
+        HIT_OBJS[i] = ','.join(cur)
+    return HIT_OBJS
 
 if __name__ == '__main__':
     NEW_FOLDER = PATH_TO_BEATMAP_FOLDER + " - REVERSED BY USO"
@@ -40,6 +64,8 @@ if __name__ == '__main__':
         tx = open(osu).read().split("\n")
         BEATMAP_ID = -1
         TITLE = -1
+        TIMING_IND = -1
+        TIMING_OBJS = []
         HIT_IND = -1
         HIT_OBJS = []
         count = 0
@@ -50,12 +76,18 @@ if __name__ == '__main__':
                 SET_ID = line.split(":")[1]
             elif "Title:" in line:
                 TITLE = line.split(":")[1]
+            elif "[TimingPoints]" in line:
+                TIMING_IND = count
             elif "[HitObjects]" in line:
                 HIT_IND = count
-            elif HIT_IND != -1:
+            elif TIMING_IND != -1 and HIT_IND == -1 and line != "":
+                TIMING_OBJS.append(line)
+            elif HIT_IND != -1 and line != "":
                 HIT_OBJS.append(line)
             count += 1
-        tx = tx[:HIT_IND + 1] + HIT_OBJS[::-1]
+        rearrangeTimingPoints(TIMING_OBJS)
+        HIT_OBJS = rearrangeHitObjs(HIT_OBJS)
+        tx = tx[:TIMING_IND + 1] + TIMING_OBJS[::-1] + ['', '[HitObjects]'] + HIT_OBJS
         tx = '\n'.join(tx)
         tx = tx.replace(SET_ID, NEW_SET_ID).replace(BEATMAP_ID, NEW_ID).replace(TITLE, "REVERSED BY USO - " + TITLE)
         open(NEW_OSU, 'w').write(tx)
